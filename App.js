7167d296e080e34dae7f7f00908fd060b5b47f1f
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-06-18 10:25:49 +0530

    Adds constant variable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // import 'react-native-gesture-handler';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
@@ -18,13 +18,32 @@ import Categories from './Components/Categories';
 import Myprofile from './Pages/Myprofile';
 import About from './Pages/About';
 
+import global from './Constants/variables';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loggedIn: false,
+    };
+    this.setToken();
+  }
+
+  async setToken() {
+    try {
+      const accessToken = await AsyncStorage.getItem('token');
+      if (accessToken != undefined || accessToken != null) {
+        global.accessToken = accessToken;
+        global.clt_type = await AsyncStorage.getItem('clt-type-id');
+        this.setState({loggedIn: true});
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
+    const {loggedIn} = this.state;
     const Stack = createStackNavigator();
 
     const MyTheme = {
@@ -41,8 +60,8 @@ export default class App extends React.Component {
           screenOptions={{
             headerShown: false,
           }}>
-          <Stack.Screen name="Signin" component={Signin} />
           <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Signin" component={Signin} />
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Forgot" component={Forgot} />
           <Stack.Screen name="Eua" component={Eua} />
@@ -58,116 +77,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
-
-// import React from 'react';
-// import type {Node} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const App: () => Node = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.js</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
