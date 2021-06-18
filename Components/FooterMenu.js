b5e9561db117a20ca39bc2a85e0cca8b3f9d008a
Nithish06 <nithss0404@gmail.com>
2021-06-18 12:18:15 +0530

    My profile changes

--- Components/FooterMenu.js
+++ Components/FooterMenu.js
@@ -76,7 +76,7 @@ export default class FooterMenu extends React.Component {
         wishlist: {
           title: 'Wishlist',
           selected: false,
-          action: () => props.navigation.navigate('Wishlist'),
+          action: () => props.navigation.navigate('Myprofile'),
           icon: require('../Assets/icon-grey/wishlist.png'),
         },
         // signin: {
