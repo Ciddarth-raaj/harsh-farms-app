84934137c264afb491938f771c36b67d44c2a139
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-06-24 14:08:57 +0530

    Changes my profile style

--- Pages/Myprofile.js
+++ Pages/Myprofile.js
@@ -17,16 +17,14 @@ import {
 import Styles from '../Constants/styles';
 import Colors from '../Constants/colors';
 
-import Header from '../Components/Header';
 import CustomInputText from '../Components/CustomTextInput';
 import GlobalWrapper from '../Components/GlobalWrapper';
+import CustomButton from '../Components/CustomButton';
 
 export default class Myprofile extends Component {
   constructor(props) {
     super(props);
-    this.toggleSwitch = this.toggleSwitch.bind(this);
-    this.toggleSwitchConfirm = this.toggleSwitchConfirm.bind(this);
-    this.toggleSwitchCurrent = this.toggleSwitchCurrent.bind(this);
+
     this.state = {
       showPassword: true,
       showConfirmPassword: true,
@@ -49,21 +47,10 @@ export default class Myprofile extends Component {
     };
   }
 
-  toggleSwitch() {
-    this.setState({showPassword: !this.state.showPassword});
-  }
-
-  toggleSwitchConfirm() {
-    this.setState({showConfirmPassword: !this.state.showConfirmPassword});
-  }
-
-  toggleSwitchCurrent() {
-    this.setState({showCurrentPassword: !this.state.showCurrentPassword});
-  }
-
-  onPressButton = () => {
+  onEditPress = () => {
     this.setState({TextInputDisableStatus: true});
   };
+
   render() {
     const {
       name,
@@ -81,145 +68,128 @@ export default class Myprofile extends Component {
       showCurrentPassword,
     } = this.state;
     return (
-      <SafeAreaView>
-        <ScrollView>
-          <View>
-            <Header />
-            <View style={styles.wrapper}>
-              <Text style={styles.heading}>My Profile</Text>
+      <GlobalWrapper>
+        <View style={styles.wrapper}>
+          <Text style={styles.heading}>My Profile</Text>
 
-              <CustomInputText
-                label={'Account Name'}
-                style={styles.input}
-                // placeholder="Name"
-                value={account_name}
-                editable={this.state.TextInputDisableStatus}
-                onChangeText={value => this.setState({account_name: value})}
-              />
+          <Text style={styles.subHeading}>Profile Details</Text>
+          <CustomInputText
+            label={'Account Name'}
+            value={account_name}
+            editable={this.state.TextInputDisableStatus}
+            onChangeText={value => this.setState({account_name: value})}
+          />
 
-              <CustomInputText
-                label={'My name'}
-                style={styles.input}
-                // placeholder="Name"
-                value={name}
-                editable={this.state.TextInputDisableStatus}
-                onChangeText={value => this.setState({name: value})}
-              />
+          <CustomInputText
+            label={'My name'}
+            value={name}
+            editable={this.state.TextInputDisableStatus}
+            onChangeText={value => this.setState({name: value})}
+          />
 
-              <CustomInputText
-                label={'Mobile number'}
-                style={styles.input}
-                // placeholder="Name"
-                value={phone}
-                keyboardType="numeric"
-                editable={this.state.TextInputDisableStatus}
-                onChangeText={value => this.setState({phone: value})}
-              />
+          <CustomInputText
+            label={'Mobile number'}
+            value={phone}
+            keyboardType="numeric"
+            editable={this.state.TextInputDisableStatus}
+            onChangeText={value => thi.setState({phone: value})}
+          />
 
-              <Text style={styles.subHeading}>
-                (Mobile number will be used during order delivery only)
-              </Text>
+          <Text style={styles.subText}>
+            {'Mobile number will be used during order delivery only'}
+          </Text>
 
-              <CustomInputText
-                label={'Email'}
-                style={styles.input}
-                value={email}
-                editable={this.state.TextInputDisableStatus}
-                onChangeText={value => this.setState({email: value})}
-              />
+          <CustomInputText
+            label={'Email'}
+            value={email}
+            editable={this.state.TextInputDisableStatus}
+            onChangeText={value => this.setState({email: value})}
+          />
 
-              <CustomInputText
-                label={'Address'}
-                value={address}
-                numberOfLines={10}
-                multiline={true}
-                maxLength={400}
-                underlineColorAndroid="transparent"
-                onChangeText={value => this.setState({address: value})}
-              />
+          <CustomInputText
+            label={'Address'}
+            value={address}
+            numberOfLines={10}
+            multiline={true}
+            maxLength={400}
+            underlineColorAndroid="transparent"
+            onChangeText={value => this.setState({address: value})}
+          />
 
-              <View style={styles.textAreaContainer}>
-                <Picker
-                  selectedValue={selectedValue}
-                  editable={this.state.TextInputDisableStatus}
-                  style={styles.input}
-                  style={{borderWidth: 1, color: 'gray'}}
-                  // onValueChange={(itemValue, itemIndex) =>
-                  //   setSelectedValue(itemValue)
-                  // }
-                >
-                  <Picker.Item label="Society" value="Society" />
-                </Picker>
-              </View>
-
-              <TouchableOpacity
-                style={styles.buttonWrapper}
-                onPress={this.onPressButton}>
-                <Text style={Styles.buttonText}>Edit</Text>
-              </TouchableOpacity>
-
-              <View style={{marginTop: 40}}>
-                <Text style={styles.subHeading}>Change Password</Text>
-
-                <CustomInputText
-                  label={'Current Password'}
-                  value={currentPass}
-                  maxLength={100}
-                  onChangeText={value => this.setState({currentPass: value})}
-                  secureTextEntry={this.state.showCurrentPassword}
-                  toggleSecure={v => this.setState({showCurrentPassword: v})}
-                />
-                <CustomInputText
-                  label={'New Password'}
-                  value={newPass}
-                  maxLength={100}
-                  onChangeText={value => this.setState({newPass: value})}
-                  secureTextEntry={this.state.showPassword}
-                  toggleSecure={v => this.setState({showPassword: v})}
-                />
-
-                <CustomInputText
-                  label={'Confirm Password'}
-                  value={confirmPass}
-                  maxLength={100}
-                  onChangeText={value => this.setState({confirmPass: value})}
-                  secureTextEntry={this.state.showConfirmPassword}
-                  toggleSecure={v => this.setState({showConfirmPassword: v})}
-                />
-              </View>
-              <View
-                style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-                <TouchableOpacity style={styles.buttonWrapper}>
-                  <Text style={Styles.buttonText}>Save</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonWrapper}>
-                  <Text style={Styles.buttonText}>Cancel</Text>
-                </TouchableOpacity>
-              </View>
-
-              <Text style={styles.subHeading}>
-                Note:Password must be atleast 8 characters long with 1
-                uppercase, 1 lower case and 1 numeric character
-              </Text>
-            </View>
+          <View style={styles.textAreaContainer}>
+            <Picker
+              selectedValue={selectedValue}
+              editable={this.state.TextInputDisableStatus}
+              style={styles.input}
+              style={{borderWidth: 1, color: 'gray'}}
+              // onValueChange={(itemValue, itemIndex) =>
+              //   setSelectedValue(itemValue)
+              // }
+            >
+              <Picker.Item label="Society" value="Society" />
+            </Picker>
           </View>
-        </ScrollView>
-      </SafeAreaView>
+
+          <CustomButton onPress={this.onEditPress}>{'Edit'}</CustomButton>
+
+          <Text style={styles.subHeading}>Change Password</Text>
+
+          <CustomInputText
+            label={'Current Password'}
+            value={currentPass}
+            maxLength={100}
+            onChangeText={value => this.setState({currentPass: value})}
+            secureTextEntry={showCurrentPassword}
+            toggleSecure={v => this.setState({showCurrentPassword: v})}
+          />
+          <CustomInputText
+            label={'New Password'}
+            value={newPass}
+            maxLength={100}
+            onChangeText={value => this.setState({newPass: value})}
+            secureTextEntry={showPassword}
+            toggleSecure={v => this.setState({showPassword: v})}
+          />
+
+          <CustomInputText
+            label={'Confirm Password'}
+            value={confirmPass}
+            maxLength={100}
+            onChangeText={value => this.setState({confirmPass: value})}
+            secureTextEntry={showConfirmPassword}
+            toggleSecure={v => this.setState({showConfirmPassword: v})}
+          />
+
+          <Text style={styles.subText}>
+            {
+              'Note : Password must be atleast 8 characters long with 1 uppercase, 1 lower case and 1 numeric character'
+            }
+          </Text>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+            <CustomButton wrapperStyle={{marginRight: 10}}>
+              {'Save'}
+            </CustomButton>
+            <CustomButton wrapperStyle={{marginLeft: 10}}>
+              {'Cancel'}
+            </CustomButton>
+          </View>
+        </View>
+      </GlobalWrapper>
     );
   }
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    margin: 10,
     padding: 20,
   },
   heading: {
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 20,
-    color: 'orange',
-    marginBottom: 40,
+    marginBottom: 20,
+    color: Colors.secondary,
   },
   label: {
     color: 'grey',
@@ -286,9 +256,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   subHeading: {
-    marginBottom: 30,
+    marginBottom: 20,
+    marginTop: 20,
     fontWeight: 'bold',
     fontSize: 16,
-    color: 'grey',
+    color: Colors.secondary,
+  },
+  subText: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    color: Colors.secondary,
+    marginBottom: 10,
   },
 });
