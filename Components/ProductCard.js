1f9434375ae91acaf0511e56aface0f30f4209e7
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-06-29 08:56:08 +0530

    Modifies product card style

--- Components/ProductCard.js
+++ Components/ProductCard.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
-import colors from '../Constants/colors';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+
 import Colors from '../Constants/colors';
+
+import CustomButton from '../Components/CustomButton';
 
 export default class ProductCard extends React.Component {
   constructor(props) {
@@ -33,6 +27,7 @@ export default class ProductCard extends React.Component {
   }
 
   render() {
+    const {qty} = this.state;
     const {id, name, mrp, sp, image, tag, stock} = this.props;
     return (
       <View style={styles.wrapper}>
@@ -48,7 +43,7 @@ export default class ProductCard extends React.Component {
               }}
               style={styles.image}
             />
-            <Text style={styles.nameTextStock}>{`${stock} Left in Stock`}</Text>
+            <Text style={styles.stockText}>{`${stock} Left in Stock`}</Text>
           </View>
 
           <View style={styles.contentWrapper}>
@@ -59,33 +54,25 @@ export default class ProductCard extends React.Component {
               <Text style={styles.spText}>{`\u20A8 ${sp}`}</Text>
             </View>
 
-            {/* {QtyVisibility && ( */}
             <View style={styles.qtyDiv}>
-              <View style={styles.textViewStyleMinus}>
-                <Text
-                  style={styles.qtyButton}
-                  onPress={() => this.updateQty('sub')}>
-                  {'-'}
-                </Text>
+              <TouchableOpacity
+                style={[styles.textViewStyleMinus, styles.qtyCounter]}
+                onPress={() => this.updateQty('sub')}>
+                <Text style={styles.qtyButton}>{'-'}</Text>
+              </TouchableOpacity>
+              <View style={styles.qtyTextWrapper}>
+                <Text style={styles.qtyText}>{qty}</Text>
               </View>
-              <View style={{flex: 1, alignItems: 'center'}}>
-                <Text className={styles.price}>{this.state.qty}</Text>
-              </View>
-              <View style={styles.textViewStylePlus}>
-                <Text
-                  style={styles.qtyButton}
-                  onPress={() => this.updateQty('add')}>
-                  {'+'}
-                </Text>
-              </View>
+              <TouchableOpacity
+                style={[styles.textViewStylePlus, styles.qtyCounter]}
+                onPress={() => this.updateQty('add')}>
+                <Text style={styles.qtyButton}>{'+'}</Text>
+              </TouchableOpacity>
             </View>
-            {/* )} */}
 
-            <TouchableOpacity style={styles.button}>
-              <Text style={{color: 'white', fontWeight: 'bold'}}>
-                Add to Cart
-              </Text>
-            </TouchableOpacity>
+            <CustomButton wrapperStyle={{padding: 10}}>
+              {'Add to Cart'}
+            </CustomButton>
           </View>
         </View>
       </View>
@@ -148,8 +135,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
   },
-  nameTextStock: {
-    fontSize: 16,
+  stockText: {
+    fontSize: 14,
     color: Colors.primary,
     textAlign: 'center',
     fontWeight: 'bold',
@@ -160,52 +147,28 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'gray',
   },
-  stockText: {
-    fontSize: 18,
-  },
-  stockGreen: {
-    color: 'green',
-  },
-  stockRed: {
-    color: 'red',
-  },
-  button: {
-    backgroundColor: '#347672',
-    // backgroundColor: '#73AB00',
-    // backgroundColor: Colors.primary,
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    width: '100%',
-  },
-  price: {
-    fontWeight: 'bold',
-    color: '#0088ff',
-    flexGrow: 0,
-  },
   qtyDiv: {
     display: 'flex',
     justifyContent: 'space-around',
     alignItems: 'center',
     flexDirection: 'row',
-    borderWidth: 1,
-    borderColor: '#c9c9c9',
     marginVertical: 10,
-    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: Colors.secondary,
+    borderRadius: 12,
   },
-
   qtyButton: {
-    fontSize: 32,
+    fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
     borderRadius: 5,
+    paddingBottom: 4,
   },
   discountSectionField: {
     position: 'absolute',
     top: 5,
     left: 5,
     backgroundColor: '#FC2215',
-    // backgroundColor: '#00AF50',
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 3,
@@ -215,16 +178,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
-  textViewStylePlus: {
-    backgroundColor: '#347672',
-    // backgroundColor: '#73AB00',
+  qtyCounter: {
+    backgroundColor: Colors.secondary,
     flex: 1,
     alignItems: 'center',
   },
+  textViewStylePlus: {
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+  },
   textViewStyleMinus: {
-    backgroundColor: '#347672',
-    // backgroundColor: '#FF4634',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  qtyTextWrapper: {
     flex: 1,
-    alignItems: 'center',
+  },
+  qtyText: {
+    fontWeight: 'bold',
+    // color: Colors.secondary,
+    textAlign: 'center',
   },
 });
