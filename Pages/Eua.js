9779b07d431f83d38947c36bbe8f97b900067bc3
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-07-02 17:26:52 +0530

    Adds new eua

--- Pages/Eua.js
+++ Pages/Eua.js
@@ -14,7 +14,48 @@ export default class Eua extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Eua: 'Lorem ipsum dolor sit amet, sse cillum dolore eu, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, suconsectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, suconsectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, suconsectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, suconsectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, suconsectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecatsint occaecat cupidatat non proident, su',
+      Eua:
+        'A privacy policy is a legal statement that discloses how a party gathers, stores, and uses the personal information it collects. Personal information refers to anything that can be used to identify an individual, including Client names, phone numbers, email addresses, Society Names. These policies are used by companies and mobile app developers to stay compliant with federal laws. They fulfil the legal requirement to safeguard user privacy while also protecting the company itself from legal challenges. The contents of these policies can vary greatly depending on the industry, user demographics, governing laws and jurisdictions, and application platform. Employing the use of third party services may also affect the need for one, as well as its contents-even if the app itself does not collect user information\n' +
+        '\n' +
+        '\n' +
+        'ORDERS and DELIVERIES \n' +
+        '\n' +
+        'We have two delivery timings in a day.\n' +
+        'For those who order from 3am to 5pm, we will deliver between 6.30pm to 8.30pm.\n' +
+        'For those who order from 5pm to 3am, we will deliver between 6am to 8am.\n' +
+        'We offer 24/7 order booking facility.\n' +
+        'We do not offer a cash on delivery option.(When we provide the "COD" facility, We will let you know for sure).\n' +
+        '\n' +
+        '\n' +
+        'RETURN POLICY\n' +
+        '\n' +
+        'After delivery, the product will be delivered to return and replacement within 1 hour.\n' +
+        '\n' +
+        '\n' +
+        'ORDER CANCEL and REFUND\n' +
+        '\n' +
+        'Any order can be cancel with-in the next 1hour.\n' +
+        'After the order is cancelled then it will be automatically generated to refund.\n' +
+        'Refund will be done only for the fund excluding the processing fee.\n' +
+        '\n' +
+        '\n' +
+        'OFFERS\n' +
+        '\n' +
+        'Occasionally we make offers. like 20% or 150grams.\n' +
+        '\n' +
+        '\n' +
+        'Changes to This Terms and Conditions :\n' +
+        '\n' +
+        'We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.\n' +
+        '\n' +
+        '(The Company reserves the right to change these terms and conditions in any manner from time to time without being liable to assign any reason for the same or to inform the Customer or any third party about such changes).\n' +
+        '\n' +
+        'These terms and conditions are effective as of 11-06-2021.\n' +
+        '\n' +
+        '\n' +
+        'Contact Us :\n' +
+        '\n' +
+        'If you have any questions or suggestions about our Terms and Conditions, do not hesitate to contact us at chandrasekharkhajana345@gmail.com.',
     };
   }
 
